cc48465eb8eb45b55dc5e16aa096acde0a46aa8e
Shiva <shivaxsin@gmail.com>
2019-11-25 13:39:09 +0800

    Wolf register

--- SheepChild/app/Http/Controllers/WolfController.php
+++ SheepChild/app/Http/Controllers/WolfController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 use App\Item;
 use App\Sheep;
 use App\Wolf;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Psy\Util\Str;
 
 class WolfController extends Controller
 {
@@ -28,7 +31,54 @@ class WolfController extends Controller
      */
     public function store(Request $request)
     {
+        // 確認是否有相同 account
+        $check_account =Wolf::where('account', $request->account)->first();
 
+        // 如果未註冊，則進入驗證資料是否符合格式跟創建會員資料
+        if($check_account == null) {
+
+            $rules = [
+                'account' => ['required', 'string', 'max:20'],
+                'password' => ['required', 'string', 'min:8', 'max:20'],
+                'name' => ['required', 'string', 'max:30'],
+            ];
+
+            $input = request()->all();
+
+            // 驗證請求資料規則是否符合
+            $validator = Validator::make($input, $rules);
+
+            if ($validator->fails()) {
+
+                return response()->json(['msg' => $validator->errors()]);
+
+            } else {
+
+                $api_token = Str::random(10);
+
+                // 店家預設註冊給 1000000 元
+                $balance = 1000000;
+                $camp_name = 'Arcadia';
+
+                // hash password
+                $HashPwd = Hash::make($request['password']);
+
+                $create = Wolf::create([
+                    'camp_name' => $camp_name,
+                    'name' => $request['name'],
+                    'account' => $request['account'],
+                    'password' => $HashPwd,
+                    'balance' => $balance,
+                    'api_token' => $api_token,
+                ]);
+
+                return response()->json([
+                    'msg' => '店家註冊成功',
+                    'create_date' => $create,
+                ]);
+
+            }
+        }
     }
 
     /**
--- SheepChild/app/Wolf.php
+++ SheepChild/app/Wolf.php
@@ -3,8 +3,16 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
 
 class Wolf extends Model
 {
-    //
+    use Notifiable;
+    protected $fillable=[
+      'camp_name','account','api_token','balance'
+    ];
+    protected $hidden=[
+      'password',
+    ];
+
 }
