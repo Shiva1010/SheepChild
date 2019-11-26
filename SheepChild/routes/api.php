264d2d571e9e57a9ca6a37b564a7ad9ea958d5dc
Shiva <shivaxsin@gmail.com>
2019-11-26 09:45:51 +0800

    pic api ok

--- SheepChild/routes/api.php
+++ SheepChild/routes/api.php
@@ -50,10 +50,11 @@ Route::post('/wolf/login','WolfController@login');
 
 // 只有狼可以新增、修改、刪除商品
 Route::group(['middleware' => ['auth:wolf']], function() {
-    Route::post('/wolf/items', 'ItemController@store');             // 店家新增商品
-    Route::put('/wolf/items/{id}', 'ItemController@update');        // 店家修改商品
-    Route::delete('/wolf/items/{id}', 'ItemController@destroy');    // 店家刪除商品
-    Route::get('/wolf/sheepitem','SheepItemController@index');      // 查看所有購買紀錄
+    Route::post('/wolf/items', 'ItemController@store');               // 店家新增商品
+    Route::put('/wolf/items/{id}', 'ItemController@update');          // 店家修改商品
+    Route::delete('/wolf/items/{id}', 'ItemController@destroy');      // 店家刪除商品
+    Route::post('/wolf/items/photo_upload', 'ItemController@upload'); // 店家已上傳商品再多新增圖片
+    Route::get('/wolf/sheepitem','SheepItemController@index');        // 查看所有購買紀錄
 });
 
 
